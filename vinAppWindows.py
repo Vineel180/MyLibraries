400fdfcf1dcae7026e90cf5161338dde80391b57
Vineel180 <vineel180@gmail.com>
2024-12-23 16:44:09 +0530

    000UJC

--- vinAppWindows.py
+++ vinAppWindows.py
@@ -1,6 +1,7 @@
 import pygetwindow
+import re
 
-def getWindow(windowName, activate=True, maximize=True, restore=True):
+def getWindow(windowName, restore=True, activate=True, maximize=True):
     window = pygetwindow.getWindowsWithTitle(windowName)
     if window:
         window = window[0]
@@ -15,3 +16,10 @@ def getWindow(windowName, activate=True, maximize=True, restore=True):
         return True
     else:
         return False
+
+def getWindow_regex(windowName_regex, restore=True, activate=True, maximize=True):
+    allWindows = pygetwindow.getAllTitles()
+    for i in allWindows:
+        if re.search(windowName_regex, i):
+            return getWindow(i, restore, activate, maximize)
+    return False
